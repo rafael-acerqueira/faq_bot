94cc30092a9b2447a5ebefbf7d9b1c5028852483
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-05-23 19:48:54 -0300

    Add relaionship between tags and links

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,7 +1,11 @@
 class Hashtag < ActiveRecord::Base
   validates_presence_of :name
-  
+
   has_many :faq_hashtags
   has_many :faqs, through: :faq_hashtags
+
+  has_many :link_hashtags
+  has_many :links, through: :link_hashtags
+
   belongs_to :company
 end
